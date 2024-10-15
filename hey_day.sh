1462a19b502726cded5d45b99676a11d0dd9206a
asmaul0223 <azh0268@auburn.edu>
2024-10-14 22:18:03 -0500

    Add files via upload

---
+++ hey_day.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script greets the users for Auburn's Hey Day
+
+# Prompt the user for their name
+echo "Please enter your name:"
+read name
+
+# Output a greeting
+echo "Hey $name! Happy Hey Day! Enjoy celebrating this Auburn tradition!"
+
